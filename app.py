4b184994d53b93ba696c96940e7d4737ee7e0a40
gitkush <38980954+gitkush@users.noreply.github.com>
2020-06-03 13:10:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pydeck as pdk
-# import plotly.express as px
+import plotly.express as px
 
 
 
@@ -63,33 +63,33 @@ st.write(pdk.Deck(
 ))
 
 
-# st.subheader("Breakdown by minute between %i:00 and %i:00" %(hour, (hour + 1) %24))
-# filtered = data[
-#     (data['date/time'].dt.hour >= hour) & (data["date/time"].dt.hour < (hour + 1))
-# ]
+st.subheader("Breakdown by minute between %i:00 and %i:00" %(hour, (hour + 1) %24))
+filtered = data[
+    (data['date/time'].dt.hour >= hour) & (data["date/time"].dt.hour < (hour + 1))
+]
 
-# # st.write(filtered)
+# st.write(filtered)
 
-# hist = np.histogram(filtered['date/time'].dt.minute, bins=60, range=(0,60))[0]
-# chart_data = pd.DataFrame({'minute': range(60), 'crashes':hist})
+hist = np.histogram(filtered['date/time'].dt.minute, bins=60, range=(0,60))[0]
+chart_data = pd.DataFrame({'minute': range(60), 'crashes':hist})
 
-# # st.write(chart_data)
+# st.write(chart_data)
 
-# fig = px.bar(chart_data, x='minute', y='crashes', hover_data = ['minute', 'crashes'], height = 400)
-# st.write(fig)
+fig = px.bar(chart_data, x='minute', y='crashes', hover_data = ['minute', 'crashes'], height = 400)
+st.write(fig)
 
 
-# st.header("Top 5 dangerous street by affected type")
-# select = st.selectbox("Affected type of people",["Pedestrians","Cyclists","Motorists"])
+st.header("Top 5 dangerous street by affected type")
+select = st.selectbox("Affected type of people",["Pedestrians","Cyclists","Motorists"])
 
-# if select == "Pedestrians":
-#     st.write(original_data.query("injured_pedestrians >= 1")[["on_street_name", "injured_pedestrians"]].sort_values(by=['injured_pedestrians'], ascending = False).dropna(how='any')[:5])
+if select == "Pedestrians":
+    st.write(original_data.query("injured_pedestrians >= 1")[["on_street_name", "injured_pedestrians"]].sort_values(by=['injured_pedestrians'], ascending = False).dropna(how='any')[:5])
 
-# elif select == "Cyclists":
-#     st.write(original_data.query("injured_cyclists >= 1")[["on_street_name", "injured_cyclists"]].sort_values(by=['injured_cyclists'], ascending = False).dropna(how='any')[:5])
+elif select == "Cyclists":
+    st.write(original_data.query("injured_cyclists >= 1")[["on_street_name", "injured_cyclists"]].sort_values(by=['injured_cyclists'], ascending = False).dropna(how='any')[:5])
     
-# else:
-#     st.write(original_data.query("injured_motorists >= 1")[["on_street_name", "injured_motorists"]].sort_values(by=['injured_motorists'], ascending = False).dropna(how='any')[:5])
+else:
+    st.write(original_data.query("injured_motorists >= 1")[["on_street_name", "injured_motorists"]].sort_values(by=['injured_motorists'], ascending = False).dropna(how='any')[:5])
 
 
 if st.checkbox("Show raw data", False):
